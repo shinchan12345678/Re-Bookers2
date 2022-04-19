f0dfb42d10f0247e24b2b80d5da90044e26f61fc
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-19 11:34:16 +0000

    [Update]Bookモデルの編集機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+  
+  def update
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    end
   end
   
   def destroy
